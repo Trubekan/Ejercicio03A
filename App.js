4c42015f19069f7c77ffa5a66ea9f7b249ca2246
Enzo <enzo.velarde@tecsup.edu.pe>
2023-04-20 20:33:43 -0500

    A

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
+import Header from './src/Header';
+import Boxes from './src/Boxes';
+
+const App =() => {
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <Header></Header> 
+     <Boxes></Boxes> 
     </View>
-  );
+  )
 }
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text, View } from 'react-native';
+
+const Header =() => {
+    return(
+        <View style={styles.container}>
+            <Text>Componente del Header</Text>   
+        </View>
+    );
+}
+
+export default Header;
+
+const styles = StyleSheet.create({
+    header:{
+        width: '100%',
+        height: '15%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#eee'
+    }
+});
